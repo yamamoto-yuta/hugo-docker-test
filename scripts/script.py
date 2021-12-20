7bb03f40e934391f7e013bc4382d5808e5da6435
yamamoto-yuta <automatuX78@gmail.com>
2021-12-21 01:03:34 +0900

    🔥 まだ使わないので一旦削除

--- scripts/script.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("issue_number", type=str, help="Issue number")
-    args = parser.parse_args()
-
-    print(args.issue_number)
